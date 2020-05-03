c31c3673708779560bcc07651e6a956670832ac4
Przemek <20Przemek777@gmail.com>
2020-05-03 23:09:23 +0200

    Dodanie wpisywania tekstu, tworzenia wpisów js

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 
 let day = ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"]
 
-function myTimer() {
+function Timer() {
 let date = new Date();
   document.getElementById("time").innerHTML = date.toLocaleTimeString();
   document.getElementById("day").innerHTML = day[date.getDay()];
 }
-myTimer()
-var myVar = setInterval(myTimer, 1000);
+Timer()
+let myVar = setInterval(Timer, 1000);
 
 // end timer
 
@@ -35,6 +35,103 @@ newEntryCircle.addEventListener('click', newEntryOpenBox);
 
 
 // Koniec otwierania panelu i zamykanie
+
+// Tworzenie nowych wpisów
+
+
+const mainEntry = document.querySelector('.main-content-entry');
+
+const creationNewEntry = (contents, title, entryCategory) => {
+
+  const entry = document.createElement('div');
+  mainEntry.appendChild(entry)
+  entry.className = "entry";
+
+  const entryTop = document.createElement('div');
+  entry.appendChild(entryTop);
+  entryTop.className = "entry-top";
+
+
+  // TYTUŁ WPISU!! TYTUŁ WPISU!!!
+  const entryTopH2 = document.createElement('h2');
+  entryTop.appendChild(entryTopH2);
+  entryTopH2.textContent = title; // - TEXT CONTENT TYTUŁU WPISU
+
+  const EntryTopRight = document.createElement('div');
+  entryTop.appendChild(EntryTopRight);
+  EntryTopRight.className = "entry-top-right";
+
+  // DATA WPISU !! DATA WPISU
+  const date = document.createElement('div');
+  EntryTopRight.appendChild(date);
+  date.className = "date";
+  date.textContent = "02/10/2021" // -- TEXT CONTENT DATY WPISU
+
+  // KATEGORIA WPISU!! KATEGORIA WPISU!!
+  const category = document.createElement('div');
+  EntryTopRight.appendChild(category);
+  category.className = "category";
+  category.textContent = entryCategory // -- TEXT CONTENT KATEGORI WPISU
+
+
+
+  const entryTopRightButtons = document.createElement('div');
+  entryTop.appendChild(entryTopRightButtons);
+  entryTopRightButtons.className = "entry-top-right-buttons";
+
+  // USUWANIE ELEMENTU!
+  const removeEntryButton = document.createElement('button');
+  entryTopRightButtons.appendChild(removeEntryButton);
+  removeEntryButton.className = "remove-entry"; 
+
+  const removeIcon = document.createElement('span');
+  removeEntryButton.appendChild(removeIcon);
+  removeIcon.className = "far fa-trash-alt";
+
+  // dodawanie do zrobionych
+  const doneEntry = document.createElement('button');
+  entryTopRightButtons.appendChild(doneEntry);
+  doneEntry.className = "done-entry";
+
+  // ICON DODAWANIA DO ZROBIONYCH
+
+  const doneIcon = document.createElement('span');
+  doneEntry.appendChild(doneIcon);
+  doneIcon.className = "far fa-check-circle";
+
+  const entryBottom = document.createElement('div');
+  entry.appendChild(entryBottom);
+  entryBottom.className = "entry-bottom";
+
+  const entryBottomTextP = document.createElement('p');
+  entryBottom.appendChild(entryBottomTextP);
+  entryBottomTextP.textContent = contents // TU JEST TEXT 
+
+
+
+}
+
+// koniec tworzenia nowych wpisów
+
+
+
+
+// Przycisk do dodawania nowych wpisów 
+
+const addEntryButton = document.querySelector('.add-new');
+
+const addEntryButtonFunction = () => {
+  const entryContents = document.querySelector('.text-area').value // pobieranie treści 
+  const entryTitle = document.querySelector('.entry-title').value // pobieranie tytułu
+  const selectCategory = document.querySelector('#select').value; // pobieranie wartości select z kategori
+
+  creationNewEntry(entryContents, entryTitle, selectCategory)
+}
+
+addEntryButton.addEventListener('click', addEntryButtonFunction);
+
+// koniec przycisku do dodawania nowych wpisów
+
 
 
 
