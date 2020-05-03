8db293e6017d814e02cf0f4edf782e894a64f4f8
Przemek <20Przemek777@gmail.com>
2020-05-03 20:46:50 +0200

    Dodanie main.js oraz modyfikowanie wyglądu

--- main.js
+++ main.js
@@ -11,3 +11,27 @@ myTimer()
 var myVar = setInterval(myTimer, 1000);
 
 // end timer
+
+// Otwieranie panelu do tworzenia notatek
+
+const newEntry = document.querySelector('.add-new-entry')
+const boxToCreateEntries = document.querySelector('.add-new-talk-container')
+const exitButton = document.querySelector('.exit');
+const newEntryCircle = document.querySelector('.add')
+
+const newEntryOpenBox = () => {
+  boxToCreateEntries.classList.toggle('active')
+}
+
+newEntry.addEventListener('click', newEntryOpenBox);
+
+const exitFunction = () => {
+  boxToCreateEntries.classList.toggle('active')
+}
+
+exitButton.addEventListener('click', exitFunction);
+
+newEntryCircle.addEventListener('click', newEntryOpenBox);
+
+
+// Koniec otwierania panelu do tworzenia notatek
